75f31506ff7aa00c69caf3011b1dafc0308b9a41
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2026-02-09 15:57:36 -0600

    hola mundo en c
    Se agregó el hola mundo en C

---
+++ 02-hola_mundo/hola-mundo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	printf("¡Hola Mundo!\n");
+	return 0;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
